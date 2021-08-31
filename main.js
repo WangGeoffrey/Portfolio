cbc5530956468fd7d895a6f0a9cd837565564403
WangGeoffrey <59940531+WangGeoffrey@users.noreply.github.com>
2021-08-31 13:21:54 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,22 @@
 var index_1 = 0;
 var index_2 = 0;
+var images_1 = document.getElementsByClassName("imgs_1");
+var images_2 = document.getElementsByClassName("imgs_2");
 
 window.onload = function() {
   cycle_1();
 }
 
 function cycle_1() {
-  var i;
-  var slides = document.getElementsByClassName("imgs_1");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  index_1++;
-  if (index_1 > slides.length) {index_1 = 1}  
-  slides[index_1-1].style.display = "block";  
+  images_1[(index_1 + images_1.length - 1) % images_1.length].style.display = "none";
+  images_1[index_1].style.display = "block";
+  index_1 = (index_1 + 1) % images_1.length;
   setTimeout(cycle_2, 2000);
 }
 
 function cycle_2() {
-  var i;
-  var slides = document.getElementsByClassName("imgs_2");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  index_2++;
-  if (index_2 > slides.length) {index_2 = 1}    
-  slides[index_2-1].style.display = "block";  
+  images_2[(index_2 + images_2.length - 1) % images_2.length].style.display = "none";
+  images_2[index_2].style.display = "block";
+  index_2 = (index_2 + 1) % images_2.length;
   setTimeout(cycle_1, 2000);
 }
