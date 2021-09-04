c467c33ef235238777863fb3cc3dfd8ce308f5d1
WangGeoffrey <59940531+WangGeoffrey@users.noreply.github.com>
2021-09-03 21:41:18 -0400

    Cycle images

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-var wait = 3000; //Time interval between cycles
+var wait = 2000; //Time interval between cycles
 var index_1 = 0;
 var index_2 = 0;
+var index_3 = 0;
 var images_1 = document.getElementsByClassName("imgs_1");
 var images_2 = document.getElementsByClassName("imgs_2");
+var images_3 = document.getElementsByClassName("imgs_3");
 
 var lastScrollTop = 0;
 var navActive = true;
@@ -10,21 +12,24 @@ var navActive = true;
 window.onload = function() {
   order(images_1);
   order(images_2);
+  order(images_3);
   setTimeout(cycle_1, wait);
 }
 
 window.addEventListener("scroll", function () {
   var st = window.pageYOffset || document.documentElement.scrollTop;
-  if (st > lastScrollTop) { //Down scroll
-    if (navActive) {
-      this.document.getElementsByTagName("nav")[0].style.transform = "translate(0, -100%)";
-      navActive = !navActive;
+  if (Math.abs(st - lastScrollTop) > 10) {
+    if (st > lastScrollTop) { //Down scroll
+      if (navActive) {
+        this.document.getElementsByTagName("nav")[0].style.transform = "translate(0, -100%)";
+        navActive = !navActive;
+      }
     }
-  }
-  else { //Up scroll
-    this.document.getElementsByTagName("nav")[0].style.transform = "translate(0, 0)";
-    if (!navActive) {
-      navActive = !navActive;
+    else { //Up scroll
+      this.document.getElementsByTagName("nav")[0].style.transform = "translate(0, 0)";
+      if (!navActive) {
+        navActive = !navActive;
+      }
     }
   }
   lastScrollTop = st <= 0 ? 0 : st;
@@ -48,5 +53,12 @@ function cycle_2() {
   images_2[index_2].style.opacity = 0;
   index_2 = (index_2 + 1) % images_2.length;
   images_2[index_2].style.opacity = 1;
+  setTimeout(cycle_3, wait);
+}
+
+function cycle_3() {
+  images_3[index_3].style.opacity = 0;
+  index_3 = (index_3 + 1) % images_3.length;
+  images_3[index_3].style.opacity = 1;
   setTimeout(cycle_1, wait);
 }
